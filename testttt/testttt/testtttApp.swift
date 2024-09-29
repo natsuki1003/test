99876cb124db14fdc34bd97bd13b31f24f0270f1
Mizusawa Natsuki <natsukimizusawa@gmail.com>
2024-09-29 19:09:08 +0900

    initialization

---
+++ testttt/testttt/testtttApp.swift
@@ -0,0 +1,17 @@
+//
+//  testtttApp.swift
+//  testttt
+//
+//  Created by Mizusawa Natsuki on 2024/09/29.
+//
+
+import SwiftUI
+
+@main
+struct testtttApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ testttt/testtttTests/testtttTests.swift
@@ -0,0 +1,17 @@
+//
+//  testtttTests.swift
+//  testtttTests
+//
+//  Created by Mizusawa Natsuki on 2024/09/29.
+//
+
+import Testing
+@testable import testttt
+
+struct testtttTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
